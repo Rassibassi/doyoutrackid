fd0de22c3895b222a6da16250b565c3efd17e402
Jack Matthews <jackhkmatthews@gmail.com>
2021-12-24 17:15:55 +0000

    feat(index): use Bananas

--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,12 +1,17 @@
 import { NextPage } from "next";
 
-import BananaNav from "../components/BananaNav/BananaNav";
+import Bananas from "../components/Bananas/Bananas";
+import { BANANA_NAV_LINKS } from "../constants/routes";
 import styles from "../styles/index.module.scss";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.root}>
-      <BananaNav className={styles.bananaNav} />
+      <Bananas
+        className={styles.bananaNav}
+        links={BANANA_NAV_LINKS}
+        shouldJiggle
+      />
     </div>
   );
 };
