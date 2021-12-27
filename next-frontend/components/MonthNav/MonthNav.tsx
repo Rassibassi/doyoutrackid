aa054f95c0c43d5d475c11a7455827d10fdef0aa
Jack Matthews <jackhkmatthews@gmail.com>
2021-12-27 23:52:49 +0000

    feat(MonthNav): create component

---
+++ next-frontend/components/MonthNav/MonthNav.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+import PillCta from "../PillCta/PillCta";
+import { ARCHIVE_YEARS } from "../../constants/dates";
+import { ROUTE_HREF } from "../../constants/routes";
+import { EPolyAs } from "../PolyCta/PolyCta";
+
+import styles from "./MonthNav.module.scss";
+
+interface IMonthNavProps {
+  activeMonth: string;
+  activeYear: string;
+  className?: string;
+}
+
+const MonthNav = ({ activeMonth, activeYear, className }: IMonthNavProps) => {
+  const rootStyles = [styles.root];
+  if (className) rootStyles.push(className);
+
+  return (
+    <nav className={rootStyles.join(" ")}>
+      {ARCHIVE_YEARS.map((year) => (
+        <div key={year.year}>
+          <h2 className={styles.year}>{year.year}</h2>
+          <ul className={styles.navList}>
+            {Array.from(year.months.values()).map((month) => (
+              <li key={month.month} className={styles.navItem}>
+                <Link
+                  href={{
+                    pathname: `/${ROUTE_HREF.archive}`,
+                    query: {
+                      year: `${year.year}`,
+                      month: `${month.month}`,
+                    },
+                  }}
+                  passHref
+                >
+                  <PillCta
+                    className={styles.pill}
+                    isActive={
+                      year.year === activeYear && month.month === activeMonth
+                    }
+                    as={EPolyAs.anchor}
+                  >
+                    {month.month}
+                  </PillCta>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default MonthNav;
