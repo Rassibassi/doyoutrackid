9b3ff2c18108a1d8d75715c1224bab9176254944
Jack Matthews <jackhkmatthews@gmail.com>
2021-12-27 23:55:38 +0000

    refactor(tracks): cleanup

--- next-frontend/pages/tracks.tsx
+++ next-frontend/pages/tracks.tsx
@@ -13,7 +13,7 @@ import styles from "../styles/tracks.module.scss";
 const TracksPage: NextPage = () => {
   const { query, replace, isReady } = useRouter();
   const isValidQuery = !!query.date;
-  const date = parseISO(String(query.date));
+  const activeDate = parseISO(String(query.date));
 
   useEffect(() => {
     if (!isValidQuery && isReady) {
@@ -34,17 +34,17 @@ const TracksPage: NextPage = () => {
     <section className={styles.root}>
       <Bananas className={styles.bananas} />
       {isValidQuery && (
-        <Tracks
-          dateQuery={format(date, "dd/LL/yyyy")}
-          className={styles.list}
-        />
-      )}
-      {isValidQuery && (
-        <DateBanana
-          className={styles.banana}
-          date={format(date, "dd/LL/yyyy")}
-          day={format(date, "EEE")}
-        />
+        <>
+          <Tracks
+            dateQuery={format(activeDate, "dd/LL/yyyy")}
+            className={styles.list}
+          />
+          <DateBanana
+            className={styles.banana}
+            date={format(activeDate, "dd/LL/yyyy")}
+            day={format(activeDate, "EEE")}
+          />
+        </>
       )}
     </section>
   );
