4617a7c8d4857b65fc1ef35ef2e9c194c272036d
Jack Matthews <jackhkmatthews@gmail.com>
2022-01-12 12:02:59 +0000

    feat(gtag): add gtag

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "normalize.css";
+import Script from "next/script";
 
 import Layout from "../components/Layout/Layout";
 import { ElevenEleven } from "../contexts/elevenEleven";
@@ -12,6 +13,20 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ElevenEleven.Provider value={{ isElevenEleven, setIsElevenEleven }}>
+      <Script
+        strategy="beforeInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-318X5P6DZZ"
+      />
+      <Script id="gtag">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          
+          gtag("config", "G-318X5P6DZZ");`}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
