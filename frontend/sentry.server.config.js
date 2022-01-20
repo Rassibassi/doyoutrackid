c698b93887e9f20b3e12c9ce2b4c645094b9cb23
Jack Matthews <jackhkmatthews@gmail.com>
2022-01-20 22:43:55 +0000

    fix(sentry): prevent on dev

--- frontend/sentry.server.config.js
+++ frontend/sentry.server.config.js
@@ -2,16 +2,20 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN || 'https://37453f480a864aac9c283b5f46d32243@o1121100.ingest.sentry.io/6157265',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-});
+if (!!process.env.NEXT_PUBLIC_INIT_SENTRY) {
+  Sentry.init({
+    dsn:
+      SENTRY_DSN ||
+      "https://37453f480a864aac9c283b5f46d32243@o1121100.ingest.sentry.io/6157265",
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0,
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+  });
+}
