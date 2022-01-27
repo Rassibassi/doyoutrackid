012b0d7e2ad65efee79a03f8648db74d0d1947c4
Jack Matthews <jackhkmatthews@gmail.com>
2022-01-27 10:10:53 +0000

    fix(sentry): reduce sample rate

--- frontend/sentry.server.config.js
+++ frontend/sentry.server.config.js
@@ -12,7 +12,7 @@ if (!!process.env.NEXT_PUBLIC_INIT_SENTRY) {
       SENTRY_DSN ||
       "https://37453f480a864aac9c283b5f46d32243@o1121100.ingest.sentry.io/6157265",
     // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.2,
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
