40f80af01a77af79e4b5abd28cb5572f0f21ddf0
Rasmus Jones <rasjones1990@gmail.com>
2022-04-24 20:02:16 +0100

    implement audd stream IDing

--- backend/app.py
+++ backend/app.py
@@ -1,51 +1,25 @@
 import csv
-import json
 import logging
 import os
 import re
-import shlex
-import subprocess
 from datetime import date, datetime
 from functools import partial
 from io import StringIO
 
 import boto3
 import pytz
-import requests
 from boto3.dynamodb.conditions import Key
-from chalice import Chalice, CORSConfig, Cron, Response
+from chalice import Chalice, CORSConfig, Response
 
 # from ENV vars
-STREAM_URL = os.environ["STREAM_URL"]
-AUDD_API_KEY = os.environ["AUDD_API_KEY"]
-AUDD_URL = os.environ["AUDD_URL"]
-FRONTEND_ORIGIN = os.environ["FRONTEND_ORIGIN"]
+# import secrets; print(secrets.token_urlsafe(24))
+AUDD_CALLBACK_SECRET = os.environ["AUDD_CALLBACK_SECRET"]
 DYNAMODB_TABLE = os.environ["DYNAMODB_TABLE"]
-
-# audd errors
-AUDD_ERROR = {
-    907: "#907 - Recognition the song is not yet released.",
-    901: "#901 - No api_token passed.",
-    900: "#900 - Wrong API token.",
-    600: "#600 - Incorrect audio url.",
-    700: "#700 - No file attached.",
-    500: "#500 - Incorrect audio file.",
-    400: "#400 - Too big audio file.",
-    300: "#300 - Fingerprinting error.",
-    100: "#100 - An unknown error. Contact audd.io.",
-}
-
-# variables for recording
-record_time = 20
-bps = 16000
-max_retries = 4
-ffmpeg_command = "/opt/bin/ffmpeg -i {} -f mp3 -t {} -"
-command = shlex.split(ffmpeg_command.format(STREAM_URL, record_time))
 
 # chalice app
 app = Chalice(app_name="doyoutrackid")
 app.log.setLevel(logging.DEBUG)
-cors_config = CORSConfig(allow_origin=FRONTEND_ORIGIN)
+cors_config = CORSConfig(allow_origin="*")
 
 # dynamo db
 dynamodb = boto3.resource("dynamodb")
@@ -54,6 +28,7 @@ table = dynamodb.Table(DYNAMODB_TABLE)
 # misc
 date_format = "%Y-%m-%d"
 datetime_format = "%Y-%m-%d %H:%M:%S%z"
+audd_datetime_format = "%Y-%m-%d %H:%M:%S"
 
 # regex sub partial to remove everything that
 # is in brackets "() []" and
@@ -61,28 +36,27 @@ datetime_format = "%Y-%m-%d %H:%M:%S%z"
 # keep spaces
 clean_string = partial(re.sub, re.compile(r"\([^)]*\)|\[[^)]*\]|[^a-zA-Z0-9\s]"), "")
 
+london_tz = pytz.timezone("Europe/London")
+audd_tz = pytz.timezone("Etc/GMT-3")
+
 
 # helper functions
 def as_london(datetime_obj):
-    return datetime_obj.astimezone(pytz.timezone("Europe/London"))
+    return datetime_obj.astimezone(london_tz)
 
 
 def get_tracks_of(start_date_obj, end_date_obj=None, reverse=True):
     if end_date_obj is None:
-        end_date_obj = start_date_obj
+        # query database table for entries of date_obj, single day
+        filtering_exp = Key("played_date").eq(start_date_obj.strftime(date_format))
+        db_response = table.query(KeyConditionExpression=filtering_exp)
 
-    # get datetime objects from start and end of date_obj
-    start_of_day = datetime.combine(start_date_obj, datetime.min.time())
-    start_of_day = as_london(start_of_day)
-
-    end_of_day = datetime.combine(end_date_obj, datetime.max.time())
-    end_of_day = as_london(end_of_day)
-
-    # scan database table for entries of date_obj
-    fe = Key("played_datetime").between(
-        start_of_day.strftime(datetime_format), end_of_day.strftime(datetime_format)
-    )
-    db_response = table.scan(FilterExpression=fe)
+    else:
+        # scan database table for entries of date_obj, range of days
+        filtering_exp = Key("played_date").between(
+            start_date_obj.strftime(date_format), end_date_obj.strftime(date_format)
+        )
+        db_response = table.scan(FilterExpression=filtering_exp)
 
     # sort by datetime
     db_response["Items"].sort(
@@ -93,11 +67,15 @@ def get_tracks_of(start_date_obj, end_date_obj=None, reverse=True):
     return db_response["Items"]
 
 
-def check_duplicate(track_a, track_b, compare_keys=["artist", "title"]):
+def is_duplicate(track_a, track_b, compare_keys=["artist", "title"]):
     return all(
         clean_string(track_a[key]).strip() == clean_string(track_b[key]).strip()
         for key in compare_keys
     )
+
+
+def any_duplicate(new_track, tracks_of_today):
+    return any(is_duplicate(new_track, saved_track) for saved_track in tracks_of_today)
 
 
 # lambda functions
@@ -161,70 +139,55 @@ def as_csv(sday, smonth, syear, eday, emonth, eyear):
     return Response(body=csv_string, headers=headers)
 
 
-@app.schedule(Cron("1/2", "9-11", "?", "*", "MON-FRI", "*"))
-def use_ffmpeg(event):
-    for retries in range(1, max_retries + 1):
-        # datetime object for later
+@app.route(
+    f"/{AUDD_CALLBACK_SECRET}",
+    methods=["POST"],
+    cors=cors_config,
+    content_types=["application/json"],
+)
+def audd_callback():
+    response = app.current_request.json_body
+
+    if response["status"] == "success":
+        result = response["result"]
+
         now_london = as_london(datetime.now())
+        audd_datetime = datetime.strptime(result["timestamp"], audd_datetime_format)
+        audd_london = as_london(audd_tz.localize(audd_datetime))
 
-        # record stream and pipe bytes into byte object
-        p = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # choose track with highest score
+        n_results = len(result["results"])
+        result["results"].sort(
+            reverse=True,
+            key=lambda item: item["score"],
+        )
+        new_track = result["results"][0]
+        app.log.debug(f"new_track: {new_track}")
 
-        app.log.debug(f"len(p.stdout): {len(p.stdout)}")
+        # get latest tracks from database to check if duplicate
+        tracks_of_today = get_tracks_of(date.today())
 
-        # check if something was recorded
-        if len(p.stdout) < bps * record_time:
-            app.log.debug(f"recording error, retrying... {retries}/{max_retries}")
-            continue
+        # check for duplicate here
+        if tracks_of_today and any_duplicate(new_track, tracks_of_today):
+            return {"message": "this was a duplicate"}
         else:
-            # request audd API with API key and recorded data
-            result = requests.post(
-                AUDD_URL, data={"api_token": AUDD_API_KEY}, files={"file": p.stdout}
-            )
-            audd_response = json.loads(result.text)
+            # write new track into database
+            item = {
+                "received_datetime": now_london.strftime(datetime_format),
+                "played_date": audd_london.strftime(date_format),
+                "played_datetime": audd_london.strftime(datetime_format),
+                "out_of": n_results,
+            }
+            item.update(new_track)
+            table.put_item(Item=item)
 
-            app.log.debug(f"audd_response: {audd_response}")
+            return {
+                "message": "added new track",
+                "today": tracks_of_today,
+                "new_track": new_track,
+            }
+    else:
+        error = response["notification"]["notification_message"]
+        app.log.debug(error)
 
-            # audd error handling
-            if audd_response["status"] == "error":
-                error_code = int(audd_response["error"]["error_code"])
-                error_message = audd_response["error"]["error_message"]
-                error_message_short = AUDD_ERROR[error_code]
-
-                # these two error codes are worth trying again
-                if error_code == 300 or error_code == 500:
-                    app.log.debug(f"audd error, retrying... {retries}/{max_retries}")
-                    continue
-                else:
-                    return {"message": error_message_short}
-
-            elif audd_response["status"] == "success" and audd_response["result"]:
-                # get latest track from database to check if duplicate
-                today = date.today()
-                tracks_of_today = get_tracks_of(today)
-                # get latest item from list if any, otherwise None
-                latest_track = next(iter(tracks_of_today), None)
-                new_track = audd_response["result"]
-
-                app.log.debug(f"latest_track: {new_track}")
-                app.log.debug(f"latest_track: {latest_track}")
-
-                if latest_track is not None and check_duplicate(
-                    new_track, latest_track
-                ):
-                    return {"message": "this was a duplicate"}
-                else:
-                    # write new track into database
-                    item = {"played_datetime": now_london.strftime(datetime_format)}
-                    item.update(new_track)
-                    db_put_response = table.put_item(Item=item)
-                    return {
-                        "message": "added new track",
-                        "item": item,
-                        "database": db_put_response,
-                    }
-
-            else:
-                return {"message": "could not find any id"}
-
-    return {"message": f"tried it {max_retries} times"}
+        return {"message": f"AUDD error: {error}"}
